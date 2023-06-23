501a81ba8224ca1a544aee5d9a166ce68196103e
Rizkybudiman29 <rizkybudiman29@gmail.com>
2023-06-23 10:20:10 +0700

    first commit

---
+++ views/themes/vegefoods/about.php
@@ -0,0 +1,103 @@
+<div class="hero-wrap hero-bread"
+    style="background-image: url('<?=base_url().'views/themes/'.theme_active().'/';?>images/awal2.jpg');">
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate text-center">
+                <p class="breadcrumbs"><span class="mr-2"><a href="<?=base_url();?>">Beranda</a></span>&gt;
+                    <span>Tentang Kami</span>
+                </p>
+                <h1 class="mb-0 bread">Tentang Kami</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+<section class="ftco-section ftco-no-pb ftco-no-pt bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center"
+                style="background-image: url(<?=base_url().'views/themes/'.theme_active().'/';?>images/awal2.jpg);">
+                <!-- <a href=""
+                    class="icon popup-vimeo d-flex justify-content-center align-items-center">
+                    <span class="icon-play"></span>
+                </a> -->
+            </div>
+            <div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+                <div class="heading-section-bold mb-4 mt-md-5">
+                    <div class="ml-md-0">
+                        <h2 class="mb-4">Welcome to Penka Studios</h2>
+                    </div>
+                </div>
+                <div class="pb-md-5">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed nisi orci. Vestibulum vestibulum fermentum dui, vitae ullamcorper ipsum imperdiet id. Nunc ligula metus, tristique sit amet ex sit amet, scelerisque congue nibh. In eget elit dolor. Donec id pellentesque velit, non porta magna. In id sapien augue. Etiam at lectus ut mi vehicula suscipit.</p>
+                    <p>Curabitur id elit at libero auctor ornare finibus porttitor dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi quis sem nisl. Donec aliquam lorem eu sapien pellentesque, non semper dui maximus. Etiam eu metus a odio tincidunt tincidunt. Mauris blandit magna nulla, et ultrices tellus fermentum sit amet.</p>
+                    <p><a href="<?=base_url('public/product');?>" class="btn btn-primary">Shop now</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- 
+<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
+    <div class="container py-4">
+        <div class="row d-flex justify-content-center py-5">
+            <div class="col-md-6">
+                <h2 style="font-size: 22px;" class="mb-0">Subcribe to our Newsletter</h2>
+                <span>Get e-mail updates about our latest shops and special offers</span>
+            </div>
+            <div class="col-md-6 d-flex align-items-center">
+                <form action="#" class="subscribe-form">
+                    <div class="form-group d-flex">
+                        <input type="text" class="form-control" placeholder="Enter email address">
+                        <input type="submit" value="Subscribe" class="submit px-3">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- <section class="ftco-section ftco-counter img" id="section-counter"
+    style="background-image: url(<?=base_url().'views/themes/'.theme_active().'/';?>images/awal2.jpg);">
+    <div class="container">
+        <div class="row justify-content-center py-5">
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="<?=money(count(produk()));?>">0</strong>
+                                <span>Produk</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="<?=money(count(produk_kategori()));?>">0</strong>
+                                <span>Kategori</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="<?=money($totalorder);?>">0</strong>
+                                <span>Transaksi</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="<?=money($totalorder);?>">0</strong>
+                                <span>Pelanggan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
